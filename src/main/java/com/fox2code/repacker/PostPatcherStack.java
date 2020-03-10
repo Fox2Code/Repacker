990f5588f16fc4781d554415a5844cd009212ea2
Fox2Code <fox2code@gmail.com>
2020-03-10 13:58:16 +0100

    Improve modularity of PostPatcher in Repacker

---
+++ src/main/java/com/fox2code/repacker/PostPatcherStack.java
@@ -0,0 +1,61 @@
+package com.fox2code.repacker;
+
+import org.objectweb.asm.ClassVisitor;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class PostPatcherStack implements PostPatcher, Iterable<PostPatcher> {
+    private final LinkedList<PostPatcher> linkedList = new LinkedList<>();
+
+    public PostPatcherStack() {}
+
+    public PostPatcherStack(PostPatcher... patchers) {
+        for (PostPatcher postPatcher:patchers) {
+            this.addPatcher(postPatcher);
+        }
+    }
+
+    public PostPatcherStack(Iterable<PostPatcher> patchers) {
+        for (PostPatcher postPatcher:patchers) {
+            this.addPatcher(postPatcher);
+        }
+    }
+
+    @Override
+    public ClassVisitor patch(ClassVisitor classVisitor) {
+        for (PostPatcher postPatcher: linkedList) {
+            classVisitor = postPatcher.patch(classVisitor);
+        }
+        return classVisitor;
+    }
+
+    @Override
+    public void post(Map<String, byte[]> remapJar) {
+        for (PostPatcher postPatcher: linkedList) {
+            postPatcher.post(remapJar);
+        }
+    }
+
+    @Override
+    public Iterator<PostPatcher> iterator() {
+        return linkedList.iterator();
+    }
+
+    public void addPatcher(PostPatcher postPatcher) {
+        if (postPatcher != null && postPatcher != PostPatcher.NONE) {
+            linkedList.add(postPatcher);
+        }
+    }
+
+    public static PostPatcher stack(PostPatcher... patchers) {
+        if (patchers.length == 0) {
+            return PostPatcher.NONE;
+        } else if (patchers.length == 1) {
+            return patchers[0];
+        } else {
+            return new PostPatcherStack(patchers);
+        }
+    }
+}
--- src/main/java/com/fox2code/repacker/Repacker.java
+++ src/main/java/com/fox2code/repacker/Repacker.java
@@ -36,6 +36,10 @@ public class Repacker {
     }
 
     public void repackClient(String version) throws IOException {
+        this.repackClient(version, null);
+    }
+
+    public void repackClient(String version,PostPatcher postPatcher) throws IOException {
         File versionIndex = dirLayout.getVersionIndexFile(version);
         File versionJar = dirLayout.getMinecraftFile(version, true);
         File versionMappings = dirLayout.getMappingFile(version, true);
@@ -56,11 +60,15 @@ public class Repacker {
             this.out.println("Parsing mapping...");
             ClientAnnotationPatcher clientAnnotationPatcher = new ClientAnnotationPatcher(mapping, mappingSrv);
             this.out.println("Remapping client jar...");
-            mapping.remap(versionJar, versionJarRemap, new LogPatcher(clientAnnotationPatcher, "client"));
+            mapping.remap(versionJar, versionJarRemap, new LogPatcher(clientAnnotationPatcher, postPatcher, "client"));
         }
     }
 
     public void repackServer(String version) throws IOException {
+        this.repackServer(version, null);
+    }
+
+    public void repackServer(String version,PostPatcher postPatcher) throws IOException {
         File versionIndex = dirLayout.getVersionIndexFile(version);
         File versionJar = dirLayout.getMinecraftFile(version, false);
         File versionMappings = dirLayout.getMappingFile(version, false);
@@ -77,7 +85,7 @@ public class Repacker {
             }
             Mapping mapping = getMappings(versionMappings, downloads.getAsJsonObject("server_mappings").get("url").getAsString(), "server");
             this.out.println("Remapping server jar...");
-            mapping.remap(versionJar, versionJarRemap, new LogPatcher("server"));
+            mapping.remap(versionJar, versionJarRemap, new LogPatcher(postPatcher, "server"));
         }
     }
 
@@ -189,27 +197,50 @@ public class Repacker {
 
     private class LogPatcher implements PostPatcher {
         private PostPatcher postPatcher;
+        private PostPatcher postPatcherSec;
         private String side;
 
         private LogPatcher(String side) {
             this.postPatcher = null;
+            this.postPatcherSec = null;
             this.side = side;
         }
 
         private LogPatcher(PostPatcher postPatcher,String side) {
             this.postPatcher = postPatcher;
+            this.postPatcherSec = null;
+            this.side = side;
+        }
+
+        private LogPatcher(PostPatcher postPatcher, PostPatcher postPatcherSec,String side) {
+            if (postPatcher == null && postPatcherSec != null) {
+                this.postPatcher = postPatcherSec;
+                this.postPatcherSec = null;
+            } else {
+                this.postPatcher = postPatcher;
+                this.postPatcherSec = postPatcherSec;
+            }
             this.side = side;
         }
 
         @Override
         public ClassVisitor patch(ClassVisitor classVisitor) {
-            return postPatcher == null ? classVisitor : postPatcher.patch(classVisitor);
+            if (postPatcher != null) {
+                classVisitor = postPatcher.patch(classVisitor);
+                if (postPatcherSec != null) {
+                    classVisitor = postPatcherSec.patch(classVisitor);
+                }
+            }
+            return classVisitor;
         }
 
         @Override
         public void post(Map<String, byte[]> remapJar) {
             if (postPatcher != null) {
                 postPatcher.post(remapJar);
+                if (postPatcherSec != null) {
+                    postPatcherSec.post(remapJar);
+                }
             }
             out.println("Writing "+side+" jar...");
         }
